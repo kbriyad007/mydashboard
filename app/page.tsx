2629750e34b599522a48bb6a9352f304100cd28f
kbriyad007 <kb.riyad007@gmail.com>
2025-04-24 00:47:08 +0600

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,16 @@
+// pages/index.tsx or pages/dashboard.tsx (depending on where you want this page)
+
+import React from "react";
+import RequestTable from "../components/RequestTable"; // Import the RequestTable component
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <RequestTable /> {/* This renders the RequestTable component */}
+    </div>
+  );
+};
+
+export default DashboardPage;
 
