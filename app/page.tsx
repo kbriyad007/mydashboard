c2d40bedd3c30fe6a7c6b3a983a2f0e44913c774
kbriyad007 <kb.riyad007@gmail.com>
2025-04-26 01:45:03 +0600

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import UserRequests from "../components/UserRequests";
 export default function Home() {
   return (
     <div className="p-6 max-w-7xl mx-auto bg-white min-h-screen space-y-6">
-      <h1 className="text-3xl font-bold text-gray-900 mb-6">User Requests</h1>
       {/* Render the UserRequests component */}
       <UserRequests />
     </div>
