bb1566f8b0e11a95c9a6cb7b6087bfb597c3dbc3
kbriyad007 <kb.riyad007@gmail.com>
2025-04-26 00:57:15 +0600

    LoadingSpinner.tsx

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 const LoadingSpinner = () => {
   return (
-    <div className="flex justify-center items-center">
-      <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+    <div className="flex justify-center items-center py-10">
+      <div className="w-6 h-6 border-2 border-gray-300 border-t-blue-600 rounded-full animate-spin" />
     </div>
   );
 };
