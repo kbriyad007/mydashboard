3aa8e7e1534b0843f1c601ff49f5df05a6365833
kbriyad007 <kb.riyad007@gmail.com>
2025-04-26 21:56:58 +0600

    Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,19 +1,36 @@
 "use client";
 
-import UserRequests from "../components/UserRequests";
-import Sidebar from "../components/sidebar";
+import Link from "next/link";
 
-export default function Home() {
+const Sidebar = () => {
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      {/* Sidebar */}
-      <Sidebar />
-
-      {/* Main Content */}
-      <div className="ml-36 flex-1 p-6"> {/* Push content after fixed sidebar */}
-        <UserRequests />
+    <div className="fixed top-0 left-0 h-full w-36 bg-gray-800 text-white p-4 flex flex-col">
+      <div className="mb-8">
+        <h2 className="text-xl font-bold">Dashboard</h2>
       </div>
+      <nav className="flex-1">
+        <ul className="space-y-5">
+          <li>
+            <Link href="/user-requests" className="text-base hover:text-gray-300 block">
+              User Requests
+            </Link>
+          </li>
+          <li>
+            <Link href="/user-data" className="text-base hover:text-gray-300 block">
+              User Data
+            </Link>
+          </li>
+          <li>
+            <Link href="/other-section" className="text-base hover:text-gray-300 block">
+              Other Section
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
-}
+};
+
+export default Sidebar;
+
 
