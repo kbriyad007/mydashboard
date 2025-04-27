f8852ad7ff7ecc8468cdbd58d80d6063c94c27d2
kbriyad007 <kb.riyad007@gmail.com>
2025-04-27 22:35:26 +0600

    page.tsx

--- app/user-data/page.tsx
+++ app/user-data/page.tsx
@@ -1,12 +1,16 @@
 "use client";
-import Sidebar from "../components/Sidebar";
 
+import Sidebar from "../components/Sidebar";
 
 export default function UserDataPage() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">User Data</h1>
-      <p className="text-lg">This is the User Data page content.</p>
+    <div className="flex">
+      <Sidebar />
+      <div className="ml-48 p-8 flex-1">
+        <h1 className="text-3xl font-bold mb-4">User Data</h1>
+        <p className="text-lg">This is the User Data page content.</p>
+      </div>
     </div>
   );
 }
+
