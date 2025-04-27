6251108316e632a401f7606232035ad4de1c876e
kbriyad007 <kb.riyad007@gmail.com>
2025-04-27 22:28:15 +0600

    page.tsx

---
+++ app/user-data/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function UserDataPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">User Data</h1>
+      <p className="text-lg">This is the User Data page content.</p>
+    </div>
+  );
+}
