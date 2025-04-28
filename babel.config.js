574988a943e6028b50fbd9f930585157241d4835
kbriyad007 <kb.riyad007@gmail.com>
2025-04-28 23:25:08 +0600

    babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    'next/babel',  // Next.js default Babel preset
+    '@babel/preset-typescript'  // TypeScript support
+  ],
+  // You can add additional configurations here if needed
+};
