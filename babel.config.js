242d507c4191e289bb1080b2fdf7ff019cc2ae5a
kbriyad007 <kb.riyad007@gmail.com>
2025-04-28 23:30:31 +0600

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    'next/babel',  // Next.js default Babel preset
-    '@babel/preset-typescript'  // TypeScript support
-  ],
-  // You can add additional configurations here if needed
-};
