8f8e8d5a891a46643cfa06ec0076ec43d4b542e7
kbriyad007 <kb.riyad007@gmail.com>
2025-04-30 23:18:45 +0600

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
