c54122a7397ae954aee0bf75f67f2ee076d9e7a0
kbriyad007 <kb.riyad007@gmail.com>
2025-05-02 00:57:10 +0600

    Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -57,6 +57,78 @@ const Sidebar = () => {
           Logout
         </Button>
       </nav>
+             <nav className="space-y-4">
+        <Link href="/">
+          <Button variant="ghost" className="w-full justify-start">
+            <Home className="w-4 h-4 mr-2" />
+            Home
+          </Button>
+        </Link>
+        <Link href="/account">
+          <Button variant="ghost" className="w-full justify-start">
+            <User className="w-4 h-4 mr-2" />
+            My Account
+          </Button>
+        </Link>
+        <Link href="/settings">
+          <Button variant="ghost" className="w-full justify-start">
+            <Settings className="w-4 h-4 mr-2" />
+            Settings
+          </Button>
+        </Link>
+        <Button variant="ghost" className="w-full justify-start">
+          <LogOut className="w-4 h-4 mr-2" />
+          Logout
+        </Button>
+      </nav>
+             <nav className="space-y-4">
+        <Link href="/">
+          <Button variant="ghost" className="w-full justify-start">
+            <Home className="w-4 h-4 mr-2" />
+            Home
+          </Button>
+        </Link>
+        <Link href="/account">
+          <Button variant="ghost" className="w-full justify-start">
+            <User className="w-4 h-4 mr-2" />
+            My Account
+          </Button>
+        </Link>
+        <Link href="/settings">
+          <Button variant="ghost" className="w-full justify-start">
+            <Settings className="w-4 h-4 mr-2" />
+            Settings
+          </Button>
+        </Link>
+        <Button variant="ghost" className="w-full justify-start">
+          <LogOut className="w-4 h-4 mr-2" />
+          Logout
+        </Button>
+      </nav>
+             <nav className="space-y-4">
+        <Link href="/">
+          <Button variant="ghost" className="w-full justify-start">
+            <Home className="w-4 h-4 mr-2" />
+            Home
+          </Button>
+        </Link>
+        <Link href="/account">
+          <Button variant="ghost" className="w-full justify-start">
+            <User className="w-4 h-4 mr-2" />
+            My Account
+          </Button>
+        </Link>
+        <Link href="/settings">
+          <Button variant="ghost" className="w-full justify-start">
+            <Settings className="w-4 h-4 mr-2" />
+            Settings
+          </Button>
+        </Link>
+        <Button variant="ghost" className="w-full justify-start">
+          <LogOut className="w-4 h-4 mr-2" />
+          Logout
+        </Button>
+      </nav>
     </aside>
   );
 };
