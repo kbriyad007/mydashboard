a434a447d7f0f9fb4032b0e8d20ed159f8916533
kbriyad007 <kb.riyad007@gmail.com>
2025-05-01 14:07:57 +0600

    page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,12 @@
-// app/mypage/test/page.tsx
-
 "use client";
 
 import { Button } from "@/components/ui/button";
 
-export default function page() {
+export default function TestPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <Button>Click me</Button>
+      <Button variant="default">Click Me</Button>
     </div>
   );
 }
+
