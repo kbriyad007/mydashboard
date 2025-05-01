fa0459dbf62225038fb48ebadbb1bc78667940fe
kbriyad007 <kb.riyad007@gmail.com>
2025-05-01 13:51:16 +0600

    page.tsx

---
+++ app/test/page.tsx
@@ -0,0 +1,13 @@
+// app/mypage/test/page.tsx
+
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export default function TestPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Button>Click me</Button>
+    </div>
+  );
+}
