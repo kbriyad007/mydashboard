beb457b5f91a12628e491342ac8437beba7773a1
kbriyad007 <kb.riyad007@gmail.com>
2025-05-02 18:16:08 +0600

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,12 @@ export default function Home() {
         <main className="p-6">
           <h2 className="text-2xl font-semibold mb-4">Dashboard Content</h2>
           <div className="grid grid-cols-3 gap-4">
-    <div className="bg-white p-4 rounded shadow">Box 1</div>
-    <div className="bg-white p-4 rounded shadow">Box 2</div>
-    <div className="bg-white p-4 rounded shadow">Box 3</div>
+             <div className="bg-white p-4 rounded shadow">Box 1</div>
+             <div className="bg-white p-4 rounded shadow">Box 2</div>
+             <div className="bg-white p-4 rounded shadow">Box 3</div>
+             <div className="bg-white p-4 rounded shadow">Box 1</div>
+             <div className="bg-white p-4 rounded shadow">Box 2</div>
+             <div className="bg-white p-4 rounded shadow">Box 3</div>
   </div>
         </main>
       </div>
