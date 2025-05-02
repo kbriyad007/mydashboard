51ea9255be3a4b0b05d27290d8a9e5fbc94c3246
kbriyad007 <kb.riyad007@gmail.com>
2025-05-02 17:55:50 +0600

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,12 @@
 "use client";
 
-import Sidebar from "@/components/Sidebar"; // ✅ This will be used
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-} from "@/components/ui/dropdown-menu";
-
-import {
-  User,
-  UserCircle,
-  Settings,
-  LogOut,
-} from "lucide-react";
-
-const Navbar = () => (
-  <header className="w-full h-16 bg-white shadow flex items-center justify-between px-6 border-b">
-    <h1 className="text-lg font-medium">Navbar</h1>
-
-    {/* Dropdown Menu */}
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline">Open</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Menu</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <User className="mr-2 h-4 w-4" />
-          My Account
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <UserCircle className="mr-2 h-4 w-4" />
-          Profile
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Settings className="mr-2 h-4 w-4" />
-          Settings
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <LogOut className="mr-2 h-4 w-4" />
-          Logout
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </header>
-);
+import Sidebar from "@/components/sidebar";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen bg-gray-100">
       <Sidebar />
-  
 
       <div className="ml-60 flex-1 flex flex-col">
         <Navbar />
@@ -67,3 +18,4 @@ export default function Home() {
     </div>
   );
 }
+
