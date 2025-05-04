ff466faa78a63ed88e985be587fd7c36dbfc04ae
kbriyad007 <kb.riyad007@gmail.com>
2025-05-04 11:24:21 +0600

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import Card from "@/components/CardList";
 import UserRequests from "@/components/UserRequests";
 import TopProducts from "@/components/TopProducts";
 import { Eye, EyeOff } from "lucide-react";
-import ReactTooltip from "react-tooltip"; // Import react-tooltip
 
 export default function Home() {
   const [showChart, setShowChart] = useState(true);
@@ -31,14 +30,12 @@ export default function Home() {
                   <EyeOff
                     size={16}
                     className={iconStyle}
-                    data-tip="Hide chart" // Added tooltip
                     onClick={() => setShowChart(false)}
                   />
                 ) : (
                   <Eye
                     size={16}
                     className={iconStyle}
-                    data-tip="Show chart" // Added tooltip
                     onClick={() => setShowChart(true)}
                   />
                 )}
@@ -53,14 +50,12 @@ export default function Home() {
                   <EyeOff
                     size={16}
                     className={iconStyle}
-                    data-tip="Hide card" // Added tooltip
                     onClick={() => setShowCard(false)}
                   />
                 ) : (
                   <Eye
                     size={16}
                     className={iconStyle}
-                    data-tip="Show card" // Added tooltip
                     onClick={() => setShowCard(true)}
                   />
                 )}
@@ -75,14 +70,12 @@ export default function Home() {
                   <EyeOff
                     size={16}
                     className={iconStyle}
-                    data-tip="Hide requests" // Added tooltip
                     onClick={() => setShowRequests(false)}
                   />
                 ) : (
                   <Eye
                     size={16}
                     className={iconStyle}
-                    data-tip="Show requests" // Added tooltip
                     onClick={() => setShowRequests(true)}
                   />
                 )}
@@ -97,14 +90,12 @@ export default function Home() {
                   <EyeOff
                     size={16}
                     className={iconStyle}
-                    data-tip="Hide top products" // Added tooltip
                     onClick={() => setShowTopProducts(false)}
                   />
                 ) : (
                   <Eye
                     size={16}
                     className={iconStyle}
-                    data-tip="Show top products" // Added tooltip
                     onClick={() => setShowTopProducts(true)}
                   />
                 )}
@@ -115,11 +106,7 @@ export default function Home() {
             {/* Box 5 */}
             <div className="bg-white p-4 rounded shadow relative">
               <div className="absolute top-2 right-2">
-                <Eye
-                  size={16}
-                  className={iconStyle}
-                  data-tip="Box 5" // Added tooltip
-                />
+                <Eye size={16} className={iconStyle} />
               </div>
               Box 5
             </div>
@@ -127,19 +114,13 @@ export default function Home() {
             {/* Box 6 */}
             <div className="bg-white p-4 rounded shadow relative">
               <div className="absolute top-2 right-2">
-                <Eye
-                  size={16}
-                  className={iconStyle}
-                  data-tip="Box 6" // Added tooltip
-                />
+                <Eye size={16} className={iconStyle} />
               </div>
               Box 6
             </div>
           </div>
         </main>
       </div>
-
-      <ReactTooltip /> {/* Tooltip container */}
     </div>
   );
 }
