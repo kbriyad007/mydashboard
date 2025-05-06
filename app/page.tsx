bb87e28b42205bef4377d449262a4722c9b8dca6
kbriyad007 <kb.riyad007@gmail.com>
2025-05-06 12:52:33 +0600

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function HomePage() {
         const weeklySorted = Object.entries(weeklyMap)
           .sort((a, b) => (a[0] < b[0] ? -1 : 1));
 
-        const weeklyArray: TotalType[] = weeklySorted.map(([_unusedKey, total], i) => ({
+        const weeklyArray: TotalType[] = weeklySorted.map(([week, total], i) => ({
           day: `W${i + 1}`,
           total,
         }));
